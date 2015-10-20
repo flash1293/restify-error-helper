aa5cccd5527174de535d7923929495ac24fd063d
Johannes Reuter <flash1293@gmail.com>
2015-10-20 08:37:04 +0200

    changed logger-injection-method

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 module.exports = {
-  logger: require('./lib/loggerSubstitute'),
   errorFactory: require('./lib/errorFactory'),
   errorHandler: require('./lib/errorHandler'),
   uncaughtExceptionHandler: require('./lib/uncaughtExceptionHandler')
